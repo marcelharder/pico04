2286c5cf43de2dc9b5b2bae8c0f0c2e78c8631e4
marcel <m_harder88@yahoo.com>
2023-08-17 15:46:49 +0800

    begin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      HomeComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
